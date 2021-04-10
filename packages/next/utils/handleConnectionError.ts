a61c48ea2059e692ceb1776832927ffbd850ed69
Gabriele Napoli <gabrielenapoli@MBP-di-Gabriele.station>
2021-04-10 15:20:56 +0200

    fix readme

--- packages/next/utils/handleConnectionError.ts
+++ packages/next/utils/handleConnectionError.ts
@@ -8,9 +8,9 @@ import { UnsupportedChainIdError } from "@web3-react/core";
 const handleConnetionError = (error: Error): string => {
   const isWrongChain = error instanceof UnsupportedChainIdError;
   if (isWrongChain) {
-    return "You are connected to the wrong chain, switch to Mumbai";
+    return "You are connected to the wrong chain, switch to Rinkeby";
   }
-  return "There was an issue with connecting to Metamask, make sure you are using Mumbai";
+  return "There was an issue with connecting to Metamask, make sure you are using Rinkeby";
 };
 
 export default handleConnetionError;
